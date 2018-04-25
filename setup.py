f73dd22f19da119983fcbdd7dec53d730d56de81
Jim Regovich <jregovich@expedia.com>
2018-04-25 15:25:20 -0500

    Updates to author

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup
 
 setup(name='zenoss',
 
-version='0.6.3',
+version='0.6.4',
     description='Module to work with the Zenoss JSON API.',
-    author="Seth Miller",
-    author_email='seth@sethmiller.me',
-    url='https://github.com/iamseth/python-zenoss',
+    author="jim regovich",
+    author_email='jregovic@rego.org'
+    url='https://github.com/jregovic/python-zenoss',
     py_modules=['zenoss',],
     keywords = ['zenoss', 'api', 'json', 'rest'],
     test_suite='tests',
